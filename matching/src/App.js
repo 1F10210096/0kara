36a2b2ede440c04c2403e4ee2b99fecac34fc826
1F10210096 <s1F102100962@iniad.org>
2023-09-06 16:24:00 +0900

    chore

---
+++ matching/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           welcome to matching
+        </p>
+        <a
+          className="App-link"
+          href="src/login"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
