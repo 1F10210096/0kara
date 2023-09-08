11aaee839c4434e295e67d02c2a621b201b76276
1F10210096 <s1F102100962@iniad.org>
2023-09-08 23:38:13 +0900

    a

--- match/src/App.tsx
+++ match/src/App.tsx
@@ -5,7 +5,8 @@ import PageA from "./Login";
 import Register from "./subsc";
 import Menu from "./Menu";
 import RandomMatch from "./randomMatch";
-
+import Dm from "./dm";
+import Friend from "./friend";
 function App() {
   return (
     <BrowserRouter>
@@ -15,6 +16,8 @@ function App() {
         <Route path="/register" element={<Register/>} />
         <Route path="/Menu" element={<Menu/>} />
         <Route path="/randomMatch" element={<RandomMatch/>} />
+        <Route path="/dm" element={<Dm/>} />
+        <Route path="/friend" element={<Friend/>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
