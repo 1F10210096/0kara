8095666dabfacb65554eb58dc16942f482953fe7
1F10210096 <s1F102100962@iniad.org>
2023-09-11 12:50:43 +0900

    ランダムマッチとcss

--- match/src/Login.tsx
+++ match/src/Login.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
 import { Link, useNavigate } from 'react-router-dom';
 import {useNavigation} from 'react-router-dom';
 import backgroundImage from './back.jpg';
 import axios from 'axios';
+import './Home.css';
+import 'particles.js';
 const bgStyle = {
   display: 'flex',
   justifyContent: 'center',
@@ -16,14 +18,25 @@ const bgStyle = {
 };
 
 const squareStyle = {
-  width: '400px',
-  height: '340px',
+  width: '500px',
+  height: '600px',
   backgroundColor: 'white',
   padding: '20px',
+  zIndex: 1 
 };
+const h1 = {
+  width: '700px',
+  height: '30px',
+  backgroundColor: 'white',
+  padding: '40px',
+};
+
+
 
 const PageA: React.FC = () => {
   const navigate = useNavigate();
+  const particleRef = useRef<HTMLDivElement>(null);
+  const particlesRef = useRef<HTMLDivElement>(null);
   const onFinish = (values: any) => {
     axios.post('http://localhost:5000/page_a', values)
     .then(response => {
@@ -40,9 +53,93 @@ const PageA: React.FC = () => {
       console.error("Error logging in:", error);
     });
   };
+  useEffect(() => {
+
+    if ((window as any).particlesJS) {
+        (window as any).particlesJS("particles-js", {
+            particles: {
+                number: {
+                    value: 30,
+                    density: {
+                        enable: true,
+                        value_area: 1121.6780303333778
+                    }
+                },
+                color: {
+                    value: "#fff"
+                },
+                shape: {
+                    type: "image",
+                    stroke: {
+                        width: 0,
+                    },
+                    image: {
+                        src: "http://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/move02/5-6/img/flower.png",
+                        width: 120,
+                        height: 120
+                    }
+                },
+                opacity: {
+                    value: 0.06409588744762158,
+                    random: true,
+                    anim: {
+                        enable: false,
+                        speed: 1,
+                        opacity_min: 0.1,
+                        sync: false
+                    }
+                },
+                size: {
+                    value: 8.011985930952697,
+                    random: true,
+                    anim: {
+                        enable: false,
+                        speed: 4,
+                        size_min: 0.1,
+                        sync: false
+                    }
+                },
+                line_linked: {
+                    enable: false,
+                },
+                move: {
+                    enable: true,
+                    speed: 7,
+                    direction: "bottom-right",
+                    random: false,
+                    straight: false,
+                    out_mode: "out",
+                    bounce: false,
+                    attract: {
+                        enable: false,
+                        rotateX: 281.9177489524316,
+                        rotateY: 127.670995809726
+                    }
+                }
+            },
+            interactivity: {
+                detect_on: "canvas",
+                events: {
+                    onhover: {
+                        enable: false,
+                    },
+                    onclick: {
+                        enable: false,
+                    },
+                    resize: true
+                }
+            },
+            retina_detect: false
+        });
+    }
+}, []);
+  
   return (
     <div style={bgStyle}>
+          <div ref={particlesRef} id="particles-js" style={{ height: '100vh' }}></div>
+      <div id="particle" ref={particleRef}></div>;
         <div style={squareStyle}>
+        <p className="fuchidori" style={{ fontSize: '30px' }}>アカウント作成</p>
     <Form
       name="normal_login"
       className="login-form"
@@ -86,6 +183,7 @@ const PageA: React.FC = () => {
       <br /> 
       <Link to="/">back Home</Link>
     </Form>
+
     </div>
     </div>
   );
--- match/src/api/index.js
+++ match/src/api/index.js
@@ -7,7 +7,6 @@ const app = express();
 
 // MongoDBへの接続設定
 const MONGO_URI = 'mongodb+srv://koki:koki1218@atlascluster.xopoj3o.mongodb.net/'; // あなたのMongoDB URIに置き換えてください
-try {
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -32,25 +31,20 @@ app.use(cors());
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
-app.post('/page_a', (req, res) => {
-    const { username, password } = req.body;
-  
-    // データベースでユーザ名とパスワードが一致するエントリを探す
-    User.findOne({ username, password }, (err, foundUser) => {
-      if (err) {
-        res.json({ success: false, message: 'Error querying the database!' });
-        return;
-      }
-  
+app.post('/page_a', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+      const foundUser = await User.findOne({ username, password });
       if (foundUser) {
-        // 一致するエントリが見つかった場合
-        res.json({ success: true, message: 'Logged in successfully!', userId: foundUser._id });
+          res.json({ success: true, message: 'Logged in successfully!', userId: foundUser._id });
       } else {
-        // 一致するエントリが見つからなかった場合
-        res.json({ success: false, message: 'Invalid username or password!' });
+          res.json({ success: false, message: 'Invalid username or password!' });
       }
-    });
-  });
+  } catch (err) {
+      res.json({ success: false, message: 'Error querying the database!' });
+  }
+});
 app.post('/subsc', (req, res) => {
   const { username, password } = req.body;
 
@@ -66,11 +60,74 @@ app.post('/subsc', (req, res) => {
   });
 });
 
+try {
+
+//待機中の接続者のモデルを作成
+const WaitingUserSchema = new mongoose.Schema({
+  userId: String,
+  timestamp: { type: Date, default: Date.now }  // 接続した時刻
+});
+
+//待機リストにユーザーを追加するエンドポイント:
+app.post('/waiting', async (req, res) => {
+  const { userId } = req.body;
+
+  // userIdが提供されていない、またはすでに待機リストに存在する場合はエラーを返す
+  const exists = await WaitingUser.findOne({ userId });
+  if (!userId || exists) {
+    return res.status(400).json({ success: false, message: 'Invalid userId or user already in waiting list' });
+  }
+
+  const user = new WaitingUser({ userId });
+  user.save()
+    .then(savedUser => {
+      console.log(user)
+      res.json({ success: true, message: 'User added to waiting list', waitingUserId: savedUser._id });
+    })
+    .catch(err => {
+      res.json({ success: false, message: 'Error saving waiting user!' });
+    });
+});
+
+//待機中の接続者リストを取得するエンドポイント:
+app.get('/getWaitingUsers', async (req, res) => {
+  try {
+    const users = await WaitingUser.find();
+    res.json({ success: true, users });
+  } catch (err) {
+    res.json({ success: false, message: 'Error fetching waiting users!' });
+  }
+});
+
+
+//マッチングロジック
+app.get('/matchUser/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const users = await WaitingUser.find({ userId: { $ne: userId } });
+
+    if (!users.length) {
+      return res.json({ success: false, message: 'No users available for matching.' });
+    }
+
+    const matchedUser = users[0];  // シンプルな例では、最初の待機ユーザーをマッチさせます。
+
+    // マッチしたら、待機リストから2人のユーザーを削除
+    await WaitingUser.deleteOne({ userId: matchedUser.userId });
+    await WaitingUser.deleteOne({ userId });
+
+    res.json({ success: true, match: matchedUser.userId });
+  } catch (err) {
+    res.json({ success: false, message: 'Error matching user!' });
+  }
+});
+
+const WaitingUser = mongoose.model('WaitingUser', WaitingUserSchema);
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-}
-catch (err) {
-    console.log(err);
+} catch (err) {
+  console.log(err);
 }
